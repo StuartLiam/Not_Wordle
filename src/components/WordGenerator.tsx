f17c3494b567dda168da17d977a6d6f4e26acdd9
OwenBurland <77137879+OwenBurland@users.noreply.github.com>
2025-10-31 12:54:59 -0500

    Add date calculation Added a calculation to determine the number of days that have passed to use to pick a word

--- src/components/WordGenerator.tsx
+++ src/components/WordGenerator.tsx
@@ -1,7 +1,12 @@
 import Words from "./Words"
 export default function WordGenerator() {
 
-    const days = 0;
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+
+    const startMilli = new Date("October 31, 2025").valueOf();
+    const currentMilli = Date.now();
+
+    const days = Math.floor((currentMilli-startMilli)/millisecondsPerDay);
     const prime = 3001;
     const q = prime*days % Words.length;
 
