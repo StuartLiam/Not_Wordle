8ae0f8f192d932591d92c8ae435034800a80d389
Alex Burland <77137879+AlexBurland@users.noreply.github.com>
2025-12-08 19:38:02 -0600

    Added attempts variable

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ import type { state } from './types/types';
 function App() {
   const answer = WordGenerator();
   const [input, setInput] = useState<string[]>([]);
-
+  const [attempts, setAttempts] = useState(0);
   const [modalOpen, setModalOpen] = useState(false);
   const [keyboardStates, setKeyboardStates] = useState(new Map<string, state>([ 
     ["A", 0], ["B", 0], ["C", 0], ["D", 0], ["E", 0], ["F", 0], ["G", 0], ["H", 0], ["I", 0], 
     ["J", 0], ["K", 0], ["L", 0], ["M", 0], ["N", 0], ["O", 0], ["P", 0], ["Q", 0], ["R", 0], 
     ["S", 0], ["T", 0], ["U", 0], ["V", 0], ["W", 0], ["X", 0], ["Y", 0], ["Z", 0]
   ]));
+
   const addLetter = (letter:string) => {
     if(input.length < 5) {
       setInput(input.concat(letter));
@@ -41,6 +42,7 @@ function App() {
       }
       setKeyboardStates(newMap);
       setInput([])
+      setAttempts(attempts + 1);
     }
   })
 
@@ -146,6 +148,7 @@ function App() {
 
                }
            />
+      <p>Attempts:{attempts}</p>
       <p>{answer}</p>
       <Grid currInput = {input}/>
       <KeyBoard states = {keyboardStates}></KeyBoard>
